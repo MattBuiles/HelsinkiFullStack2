e280ad5daf87d185b7d1d996b850a332f3c722fe
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2025-01-12 13:32:45 -0500

    Render Test

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
 
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(express.static('dist'))
 
 
 let persons = [
@@ -81,7 +85,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
